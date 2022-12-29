77bdcbf68ea8f17beac02b56ba453c1495c85e35
NoraMohammes <NoorMohammed3399@gmail.com>
2022-12-29 16:58:37 +0200

    add drop_db file

---
+++ Scripts/Drop_DB.sh
@@ -0,0 +1,20 @@
+if [ -d ./DB_engine] ;then
+
+    cd DB_engine
+    read -p "enter Database Name " name
+
+    for i in 'ls -d'
+    do
+       if($i ==$name)
+          rm -r $i
+          echo "database deleted successfuly"
+          ./MainMenu.sh            
+       else
+          echo "there is no database with this name"
+          ../MainMenu.sh  
+    done
+    
+else
+   echo "Database engine has no databases"
+   ./MainMenu.sh
+fi
