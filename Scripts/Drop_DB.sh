ad2a2fa2d52f7c925707ec736ee3700ed5d2a52f
NoraMohammes <NoorMohammed3399@gmail.com>
2022-12-29 21:06:15 +0200

    update dropDB

--- Scripts/Drop_DB.sh
+++ Scripts/Drop_DB.sh
@@ -1,20 +1,30 @@
-if [ -d ./DB_engine] ;then
+if [ -d ./DB_engine ] ;then
 
-    cd DB_engine
-    read -p "enter Database Name " name
-
-    for i in `ls -d`
-    do
-       if($i ==$name) ;then
-          rm -r $i
-          echo "database deleted successfuly"
-          ./MainMenu.sh            
-       else
-          echo "there is no database with this name"
-          ../MainMenu.sh  
-    done
-    
-else
-   echo "Database engine has no databases"
-   ./MainMenu.sh
+    check=$(ls DB_engine | wc -l)
+    list=$(ls DB_engine)
+    if [[ $check != 0 ]] ;then
+      for i in $list
+        do
+           echo $i
+        done
+       read -p "enter Database Name " name
+       for i in $list
+         do
+          if [[ $i == $name ]] ;then
+             rm -r DB_engine/$i
+             echo "database deleted successfuly"
+             ./MainMenu.sh    
+          else
+             echo "there is no database with this name"
+             ./MainMenu.sh    
+           fi      
+         done
+     else
+      echo "Database engine has no databases"
+      ./MainMenu.sh
+     fi
+else 
+      echo "Database Engine isn't made yet for now "
+          ./MainMenu.sh  
 fi
+     
