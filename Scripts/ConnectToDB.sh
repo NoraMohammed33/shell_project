d16a90d230a706fce22a105fa3a78d4308e0e3f3
samerhassan95 <arch.samerhassan@gmail.com>
2022-12-29 19:23:59 +0200

    add table menu

--- Scripts/ConnectToDB.sh
+++ Scripts/ConnectToDB.sh
@@ -2,27 +2,28 @@
 connect_DB(){
 
     echo -e "Welcome To $DB_name Database \n"
-    select table in "Create Table" "List Tables" "Drop Table" "Insert To Table" "Select From Table" "Delete From Table" "Back To Main Menu"
-    do 
-        case $table in 
-        "Create Table" )
-         ./Create_Table.sh;;
-         "List Tables" )
-         ./List_Tables.sh;;
-         "Drop Table" )
-         ./Drop_Table.sh;;
-         "Insert To Table" )
-         ./Insert_To_Table.sh;;
-         "Select From Table" )
-         ./Select_From_Table.sh;;
-         "Delete From Table" )
-         ./Delete_From_Table.sh;;
-         "Back To Main Menu" )
-         ./MainMenu.sh;;
-         * )
-            echo -e "Please Choose From The Upper Menu \n"
-        esac
-    done
+    ./Table_menu.sh
+    # select table in "Create Table" "List Tables" "Drop Table" "Insert To Table" "Select From Table" "Delete From Table" "Back To Main Menu"
+    # do 
+    #     case $table in 
+    #     "Create Table" )
+    #      ./Create_Table.sh;;
+    #      "List Tables" )
+    #      ./List_Tables.sh;;
+    #      "Drop Table" )
+    #      ./Drop_Table.sh;;
+    #      "Insert To Table" )
+    #      ./Insert_To_Table.sh;;
+    #      "Select From Table" )
+    #      ./Select_From_Table.sh;;
+    #      "Delete From Table" )
+    #      ./Delete_From_Table.sh;;
+    #      "Back To Main Menu" )
+    #      ./MainMenu.sh;;
+    #      * )
+    #         echo -e "Please Choose From The Upper Menu \n"
+    #     esac
+    # done
 }
 if [ ! -d ./DB_engine ]
 then 
---
+++ Scripts/Table_menu.sh
@@ -0,0 +1,21 @@
+    select table in "Create Table" "List Tables" "Drop Table" "Insert To Table" "Select From Table" "Delete From Table" "Back To Main Menu"
+    do 
+        case $table in 
+        "Create Table" )
+         ./Create_Table.sh;;
+         "List Tables" )
+         ./List_Tables.sh;;
+         "Drop Table" )
+         ./Drop_Table.sh;;
+         "Insert To Table" )
+         ./Insert_To_Table.sh;;
+         "Select From Table" )
+         ./Select_From_Table.sh;;
+         "Delete From Table" )
+         ./Delete_From_Table.sh;;
+         "Back To Main Menu" )
+         ./MainMenu.sh;;
+         * )
+            echo -e "Please Choose From The Upper Menu \n"
+        esac
+    done
